5e5e4920e9309e25e651ca8520ae961ca00c9c68
gennadziy <37203741+gennadziy@users.noreply.github.com>
2020-04-21 21:51:23 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,24 @@
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/gennadziy'));
+
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/gennadziy/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000);
