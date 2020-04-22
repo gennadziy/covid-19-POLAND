4fd9c544e899da7a64fca894f69d459cee1d2e3b
gennadziy <37203741+gennadziy@users.noreply.github.com>
2020-04-22 15:42:13 +0200

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Poland1Component } from './poland1/poland1.component';
 import { BelarusComponent } from './belarus/belarus.component';
+import { PolandComponent } from './poland/poland.component';
+
 
 
 
@@ -9,6 +11,8 @@ import { BelarusComponent } from './belarus/belarus.component';
 const routes: Routes = [
   { path: 'poland1', component: Poland1Component },
   { path: 'belarus', component: BelarusComponent },
+  { path: 'poland', component: PolandComponent }
+
 
 ];
 
